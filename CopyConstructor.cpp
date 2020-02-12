a2febe2c9ad7394d0bee65c6f303bf432a006746
Sarthak1306 <sarthak13062001@gmail.com>
2020-02-12 12:03:39 +0530

    Comments

--- CopyConstructor.cpp
+++ CopyConstructor.cpp
@@ -1,27 +1,27 @@
 #include<iostream>
 using namespace std;
 
-class demo
+class demo                       //Class Declaration
 {
 private:
   int x,y;
 
 public:
-    demo(int x1, int y1)
+    demo(int x1, int y1)         //Parameterized Constructor
      {
        x = x1;
        y = y1;
      }
 
 
-    demo(const demo &d2)
+    demo(const demo &d2)         //Copy Constructor
     {
 
       x = d2.x;
       y = d2.y;
      }
 
-    int getX()
+    int getX()                   //Get Function
       {
           return x;
          }
@@ -33,8 +33,8 @@ public:
 
 int main()
 {
-    demo d1(10, 15);
-    demo d2 = d1;
+    demo d1(10, 15);          //Parameterized Constructor Call
+    demo d2 = d1;             // Copy Constructor Call
 
     cout << "d1.x = " << d1.getX() << ", d1.y = " << d1.getY();
     cout << "\nd2.x = " << d2.getX() << ", d2.y = " << d2.getY();
